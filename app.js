b89c6fac904be8988772b8fc3aeb9899e6b92365
Javier Pérez <jpv881@gmail.com>
2018-03-27 18:32:05 +0200

    actualizada estructura

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var registroRouter = require('./routes/registro');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/registro', registroRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('destinos', {
+    title: 'Agencia Viajes',
+    layout: 'layout'
+  });
 });
 
 module.exports = router;
